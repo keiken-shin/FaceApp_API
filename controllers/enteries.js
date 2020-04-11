11f89cbc9ac59f7c6e19e1ab8f37266f17824f38
AnubhavK27 <37549611+AnubhavK27@users.noreply.github.com>
2020-04-11 20:48:30 +0530

    api commit

---
+++ controllers/enteries.js
@@ -0,0 +1,33 @@
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+    apiKey: '5abfae422e084e6dbe1f92a4472e739a'
+});
+
+const handleApiCall = (req, res) => {
+    app.models.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    .then(data => {
+        res.json(data);
+    })
+}
+
+
+const handleEnteries = (req, res, db) => {
+    const { id } = req.body;
+    db('users').where('id', '=', id)
+    .increment('enteries', 1)
+    .returning('enteries')
+    .then(enteries => {
+        if(enteries){
+            res.json(enteries[0]);
+        }else{
+            res.status(400).json('No user found');
+        }
+    })
+    .catch(err => res.status(400).json('Unable to get enteries'))
+}
+
+module.exports = {
+    handleEnteries: handleEnteries,
+    handleApiCall: handleApiCall
+}
---
+++ server.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const bcrypt = require('bcryptjs');
+const cors = require('cors');
+const knex = require('knex');
+
+// Modules
+const signin = require('./controllers/signin');
+const register = require('./controllers/register');
+const profile = require('./controllers/profile');
+const enteries = require('./controllers/enteries');
+
+// Environmental Variables
+const PORT = process.env.PORT || 8000;
+
+// DATABASE Connection
+const db = knex({
+    client: 'pg',
+    connection: {
+        host: '127.0.0.1',
+        user: 'postgres',
+        password: '3127',
+        database: 'faceapp'
+    }
+})
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+// Root Route
+app.get('/', (req, res) => {
+    res.send(database.users);
+})
+
+// Sigin
+app.post('/signin', (req, res) => {
+    signin.handleSignIn(req, res, db, bcrypt);
+})
+
+// Register
+app.post('/register', (req,res) => {
+    register.handleRegister(req, res, db, bcrypt);
+})
+
+// User Profile
+app.get('/profile/:id', (req, res) => {
+    profile.handleProfile(req, res, db);
+})
+
+// Enteries Update
+app.put('/image', (req, res) => {
+    enteries.handleEnteries(req, res, db);
+})
+
+// Image Detect
+app.post('/imageurl', (req, res) => {
+    enteries.handleApiCall(req, res);
+})
+
+app.listen(PORT, () => {
+    console.log(`App on port ${PORT}`);
+})
