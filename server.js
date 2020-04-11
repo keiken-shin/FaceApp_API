5cb0f23be581d42d5e39cb2fba067ae3b7d468a0
AnubhavK27 <37549611+AnubhavK27@users.noreply.github.com>
2020-04-11 23:31:23 +0530

    Added Access-origin-allow-control

--- server.js
+++ server.js
@@ -27,7 +27,10 @@ const app = express();
 
 // Middleware
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
     next();
 });
 app.use(express.json());
