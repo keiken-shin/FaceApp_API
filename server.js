393d0563d960e3d0db49e3d6f229eac3ce0b8ce9
AnubhavK27 <37549611+AnubhavK27@users.noreply.github.com>
2020-04-11 23:43:00 +0530

    Removed Access-origin-allow-control

--- server.js
+++ server.js
@@ -26,13 +26,6 @@ const db = knex({
 const app = express();
 
 // Middleware
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    next();
-});
 app.use(express.json());
 app.use(cors());
 
